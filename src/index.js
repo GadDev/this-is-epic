b0e8a985e2b9638935803995c9e09e016e93bacd
Alex Gadaix <alex.gadaix@gmail.com>
2021-04-22 11:18:29 +0100

    scroll to top

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
+import ScrollToTop from "./helpers/ScrollToTop";
+
 ReactDOM.render(
   <Router>
+    <ScrollToTop />
     <App />
   </Router>,
   document.getElementById("root")
